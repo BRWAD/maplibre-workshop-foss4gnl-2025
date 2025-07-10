ba91d15a72c3a21b0019f4ab96a8a4e4e41344c2
BRWAD <adriaan@buro-regen-en-water.nl>
2025-07-10 10:55:22 +0000

    a

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ import * as maplibregl from "https://esm.sh/maplibre-gl";
 
 const map = new maplibregl.Map({
     container: 'mijnkaart', // container id
-    style: './assets/style.json'
-    center: [5.666467,51.972757], // starting position [lng, lat]
-    zoom: 16 // starting zoom
+    style: './assets/style.json',
+    center: [51.96857, 5.66509], // starting position [lng, lat]
+    zoom: 13 // starting zoom
 });
 
 import { Protocol } from "https://esm.sh/pmtiles";
